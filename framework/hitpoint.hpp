fe8043a6c249f1381a1c394ca9bfeaa79e1b1970
Giorgi Kusikashvili <giorgiqusikashvili1@email.com>
2026-07-28 02:11:47 +0200

    normalvektor und Material-Referenz in hitpoint.hpp (7.1)

--- framework/hitpoint.hpp
+++ framework/hitpoint.hpp
@@ -3,6 +3,9 @@
 #include "color.hpp"
 #include <glm/vec3.hpp>
 #include <string>
+#include <memory>
+
+struct Material;
 
 struct HitPoint
 {
@@ -12,4 +15,11 @@ struct HitPoint
     Color color = {0.0f, 0.0f, 0.0f};
     glm::vec3 intersection_point = {0.0f, 0.0f, 0.0f};
     glm::vec3 direction = {0.0f, 0.0f, 0.0f};
+
+    //Für das Beleuchtungsmodell. Normalvektor, der senkrecht auf der Oberfläche am Schnittpunkt steht.
+    //hilft beim berechnen von Winkel des einfallenden Lichtes
+    glm::vec3 normal = {0.0f, 0.0f, 0.0f};
+
+    //Damit wir beim Berechnen der Farbe direkt auf die Materialkoeffizienten ($K_a, K_d, K_s, m$) zugreifen können.
+    std::shared_ptr<Material> material = nullptr;
 };
